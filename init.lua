56d6cf0169587780fb4433c943fdc83a881a1b53
z-wentao <1728679818@qq.com>
2025-05-13 05:45:20 +0800

    built from scratch

---
+++ init.lua
@@ -0,0 +1,19 @@
+require("config.lazy")
+print("i will master neovim!")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+vim.cmd [[hi @function.builtin guifg=yellow]]
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
