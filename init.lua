1961d9bda77ab0b2c2c4f3c7792fad77ed7cdc8a
Zhu Wentao <1728679818@qq.com>
2025-10-27 12:47:58 +0800

    swap

--- init.lua
+++ init.lua
@@ -11,6 +11,10 @@ vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 vim.keymap.set("n", "<space>pv", ":Vex<CR>")
 
+-- Move lines up and down in visual mode (ThePrimeagen style)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
